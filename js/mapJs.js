7b7a65d2db62ec00f7af39be238e432b29ac55ba
Ayuta Tsuboi <tsuboi.asnet@gmail.com>
2014-05-14 01:09:14 +0900

    current location will be shown.

--- js/mapJs.js
+++ js/mapJs.js
@@ -7,5 +7,52 @@ function getData(){
     return localStorage.getItem("id");
    
 }
+function onBaloonclicked(){
+    document.getElementById("idtext").value="clicked";
+}
+var map = null;
+var successCallback = function(position2) {
+    var latlng = new google.maps.LatLng(position2.coords.latitude, position2.coords.longitude);
+    map.setCenter(latlng);
+    google.maps.event.addListener(map, 'click', function(event) {
+        placeMarker(event.latLng);
+    });
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: 'Current Location'
+    });
+    markersArray.push(marker);
+    google.maps.event.addListener(marker, 'click', function() {
+        onBaloonclicked();
+    });
+}
 
-
+function errorCallback(error) {
+    google.maps.event.addListener(map, 'click', function(event) {
+        placeMarker(event.latLng);
+    });
+    alert("現在位置情報を取得できひんかったよん！");
+}
+function initialize(id, pw) {
+    saveData(id, pw);
+    var mapOptions = {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 17,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+    
+    if (typeof (navigator.geolocation) == 'undefined') {
+        geolocation = google.gears.factory.create('beta.geolocation');
+    } else {
+        geolocation = navigator.geolocation;
+    }
+    var option = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0
+    };
+    geolocation.getCurrentPosition(successCallback, errorCallback, option);
+}
--- map/map.php
+++ map/map.php
@@ -10,36 +10,28 @@ $html = <<< EOL
     <head>
         <meta charset="UTF-8">
         <title></title>
-    <script type="text/javascript" src="../js/mapJs.js"></script>
+        
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <style type="text/css">
-      html { height: 100% }
-      body { height: 100%; margin: 0; padding: 0 }
-      #map_canvas { height: 100% }
-    </style>
-    <script type="text/javascript"
-      src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAXoyzE3QfIexjkyJz9T9WSgMCMtoXK2Ok&sensor=true">
-    </script>
-    <script type="text/javascript">
-      function initialize(id, pw) {
-        saveData(id, pw);
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-      }
-    </script>
+        <script type="text/javascript"
+            src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAXoyzE3QfIexjkyJz9T9WSgMCMtoXK2Ok&sensor=true">
+        </script>
+        <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="../js/mapJs.js"></script>
+        <style type="text/css">
+            html { height: 100% }
+            body { height: 100%; margin: 0; padding: 0 }
+            #map_canvas { height: 100% }
+        </style>
     </head>
     <body onLoad="initialize('$id', '$pw')">
         <form action='./map.php' method='post'>
             <input type='file' id='file' name='file'/>
             <input type='submit' value='submit'/>
         </form>
+        <input type='textarea' id='idtext' readonly="readonly"/>
         <div id="map_canvas" style="width:100%; height:100%"></div>
-        <input type='text' id='idtext'/>
         <script>
             var id = getData();
             document.getElementById("idtext").value=id;
